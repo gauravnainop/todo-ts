45ce1a6e834df9a3ab320404454afa179dec1b3b
Gaurav <itschillg@gmail.com>
2026-01-16 12:52:48 +0530

    Strated Making CreateTodo

---
+++ src/components/CreateTodo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateTodo = () => {
+  return (
+    <div className='flex bg-gray-100 w-full '>
+      <div><h1>Create Todo</h1></div>
+    </div>
+  )
+}
+
+export default CreateTodo
