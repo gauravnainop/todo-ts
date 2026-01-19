2b4d8ce0629bb6c1e1d8628cba448635e1163f26
Gaurav <itschillg@gmail.com>
2026-01-19 17:00:09 +0530

    added Delete todo functionality

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { MdDelete } from "react-icons/md";
 
 interface TodoItems {
   _id: string;
@@ -48,6 +49,21 @@ const TodoList = ({ refreshTrigger }: todoListProp) => {
     }
   };
 
+  const deleteTodo = async (
+    e: React.MouseEvent<HTMLOrSVGElement>,
+    todoId: string,
+  ) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`/api/todos/delete/${todoId}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) throw new Error("Failed to Delete todos");
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  };
+
   if (error)
     return (
       <div className="flex justify-center p-10">
@@ -68,46 +84,53 @@ const TodoList = ({ refreshTrigger }: todoListProp) => {
             {todoData?.todos.map((todo, index) => (
               <div
                 key={index}
-                className="flex items-start p-4 bg-gray-50 rounded-lg hover:bg-gray-100 transition-colors border border-transparent hover:border-gray-200"
+                className="flex items-center justify-between p-4 bg-gray-50 rounded-lg hover:bg-gray-100 transition-colors border border-transparent hover:border-gray-200"
                 onClick={(e) => {
                   markSuccess(e, todo._id);
                 }}
               >
                 {/* Status Indicator */}
-                <div
-                  className={`mt-1 h-5 w-5 rounded-full cursor-pointer border-2 shrink-0 ${
-                    todo.completed
-                      ? "bg-green-500 border-green-500"
-                      : "border-gray-300"
-                  }`}
-                >
-                  {todo.completed && (
-                    <svg
-                      className="h-4 w-4 text-white"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="3"
-                        d="M5 13l4 4L19 7"
-                      />
-                    </svg>
-                  )}
-                </div>
-
-                <div className="ml-4">
-                  <h3
-                    className={`text-lg font-medium ${todo.completed ? "text-gray-400 line-through" : "text-gray-900"}`}
+                <div className="flex items-center">
+                  <div
+                    className={`mt-1 h-5 w-5 rounded-full cursor-pointer border-2 shrink-0 ${
+                      todo.completed
+                        ? "bg-green-500 border-green-500"
+                        : "border-gray-300"
+                    }`}
                   >
-                    {todo.title}
-                  </h3>
-                  <p className="text-gray-500 text-sm mt-1">
-                    {todo.description}
-                  </p>
+                    {todo.completed && (
+                      <svg
+                        className="h-4 w-4 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="3"
+                          d="M5 13l4 4L19 7"
+                        />
+                      </svg>
+                    )}
+                  </div>
+                  <div className="ml-4">
+                    <h3
+                      className={`text-lg font-medium ${todo.completed ? "text-gray-400 line-through" : "text-gray-900"}`}
+                    >
+                      {todo.title}
+                    </h3>
+                    <p className="text-gray-500 text-sm mt-1">
+                      {todo.description}
+                    </p>
+                  </div>
                 </div>
+                <MdDelete
+                  className="text-2xl text-red-700 cursor-pointer"
+                  onClick={(e) => {
+                    deleteTodo(e, todo._id);
+                  }}
+                />
               </div>
             ))}
 
