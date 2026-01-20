54a2bdb6573ac67fa3556479eb3054ed2062fc20
Gaurav <itschillg@gmail.com>
2026-01-20 10:55:08 +0530

    Fixed Infinite Loading of TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // Import routing components
 import CreateTodo from "./components/CreateTodo";
 import Navbar from "./components/Navbar";
 import TodoList from "./components/TodoList";
+import History from "./pages/History"; // Assume this exists
 
 const App = () => {
-  const [refreshTrigger, setRefreshTrigger] = useState(0);
+  const [todoCreatedTrigger, setTodoCreatedTrigger] = useState(0);
 
   const handleTodoCreated = () => {
-    setRefreshTrigger(refreshTrigger + 1);
+    setTodoCreatedTrigger(todoCreatedTrigger + 1);
   };
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Navbar />
-      <main className="">
-        <div className="w-full flex justify-center  relative">
-          <CreateTodo onTodoCreated={handleTodoCreated} />
-          <TodoList refreshTrigger={refreshTrigger} />
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <div className="w-full flex justify-center relative">
+                  <CreateTodo onTodoCreated={handleTodoCreated} />
+                  <TodoList todoCreatedTrigger={todoCreatedTrigger} />
+                </div>
+              }
+            />
+            <Route path="/history" element={<History />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 };
 
